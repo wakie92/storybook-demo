c0be1151bb51417128dd3471f0fea1aebdc9596d
wakie92 <wakie9292@gmail.com>
2021-05-27 15:01:05 +0900

    test pre-commit

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,33 @@
-import commonjs from "rollup-plugin-commonjs";
-import resolve from "rollup-plugin-node-resolve";
-import babel from "rollup-plugin-babel";
-import pkg from "./package.json";
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
 // import external from "rollup-plugin-peer-deps-external";
-import svgr from "@svgr/rollup";
-import url from "rollup-plugin-url";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import svgr from '@svgr/rollup';
+import url from 'rollup-plugin-url';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import pkg from './package.json';
 
-const extensions = [".js", ".jsx", ".ts", ".tsx"];
+const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 
-process.env.BABEL_ENV = "production";
+process.env.BABEL_ENV = 'production';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  input: "./src/index.js",
+  input: './src/index.js',
   plugins: [
     peerDepsExternal(),
     resolve({ extensions }),
     commonjs({
-      include: "node_modules/**",
+      include: 'node_modules/**',
     }),
-    babel({ extensions, include: ["src/**/*"], runtimeHelpers: true }),
+    babel({ extensions, include: ['src/**/*'], runtimeHelpers: true }),
     url(),
     svgr(),
   ],
   output: [
     {
       file: pkg.module,
-      format: "es",
+      format: 'es',
     },
   ],
 };
